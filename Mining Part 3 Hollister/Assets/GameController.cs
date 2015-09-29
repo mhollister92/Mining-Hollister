40a253d4cb4ca2b6aa7acd19da817f19cad3fe49
mhollister92 <mhollister92@gmail.com>
2015-09-28 22:12:44 -0400

    Part 3

---
+++ Mining Part 3 Hollister/Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject cubePrefab;
+	float spawnFrequency = 3.0f;
+	float timeToAct = 0.0f;
+	float spawnSilverTime = 12.0f;
+	float stopSpawningTime = 6.0f;
+
+	// Use this for initialization
+	void Start () {
+		timeToAct += spawnFrequency;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= timeToAct && Time.time < spawnSilverTime + stopSpawningTime) {
+			GameObject myCube = (GameObject) Instantiate(cubePrefab, 
+			            new Vector3 (Random.Range (-9f,9f), Random.Range (-3f, 5f), 0), 
+			             Quaternion.identity);
+			if (Time.time >= spawnSilverTime)
+			{
+				myCube.GetComponent<Renderer>().material.color = Color.white;
+			}
+			else	
+			{
+				myCube.GetComponent<Renderer>().material.color = Color.red;
+			}
+			timeToAct += spawnFrequency;
+		}
+	
+	}
+}
